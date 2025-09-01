53f4a87ed37d1885a7de00cfd6c3effe4b7df97b
mrmorgan-i <75841431+mrmorgan-i@users.noreply.github.com>
2025-09-01 00:28:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Web;
+
+// Models for the data.
+public class TriviaResponse
+{
+    [JsonPropertyName("response_code")]
+    public int ResponseCode { get; set; }
+
+    [JsonPropertyName("results")]
+    public List<TriviaQuestion> Results { get; set; } = new();
+}
+
+public class TriviaQuestion
+{
+    [JsonPropertyName("category")]
+    public string Category { get; set; } = "";
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; } = "";
+
+    [JsonPropertyName("difficulty")]
+    public string Difficulty { get; set; } = "";
+
+    [JsonPropertyName("question")]
+    public string Question { get; set; } = "";
+
+    [JsonPropertyName("correct_answer")]
+    public string CorrectAnswer { get; set; } = "";
+
+    [JsonPropertyName("incorrect_answers")]
+    public List<string> IncorrectAnswers { get; set; } = new();
+}
